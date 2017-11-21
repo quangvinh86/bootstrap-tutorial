76cd9720213ee90bb9517f6f0c7e1fc36e4b1d8b
quangvinh86 <quangvinh19862003@gmail.com>
2017-11-21 11:14:33 +0700

    update new code in 1.js

---
+++ bootstrap-tutorial/tutorials/Website-4/1.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function(){
+    $('.navbar-right li:nth-child(1) a')
+	.click(function(){
+        var body = $("html, body");
+        body.animate({
+            scrollTop:750});
+		return false; 
+    });
+    
+	$('.navbar-right li:nth-child(2) a')
+	.click(function(){
+        var body = $("html, body");
+		body.animate({
+            scrollTop:750});
+		return false; 
+	});
+
+	$('.navbar-right li:nth-child(3) a')
+	.click(function(){
+        var body = $("html, body");
+        body.stop().animate({scrollTop:1100}, 500, 'swing', function() { 
+         });
+		return false; 
+	});
+
+});
